67a146980ae859810ded2db7d8743b1ab6293b2c
kashifaliawan <kashifalismitofficial@gmail.com>
2025-09-30 22:28:05 +0500

    learning local storage

---
+++ local_Storage/app.js
@@ -0,0 +1,9 @@
+let r_name = document.querySelector("#r-name");
+let r_email = document.querySelector("#r-email");
+let r_password = document.querySelector("#r-password");
+// Login Form
+let l_password = document.querySelector("#l-password");
+let l_email = document.querySelector("#l-email");
+
+
+r_email.
