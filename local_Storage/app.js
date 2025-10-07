f3b1b9f739717da26439d155b221b31c2be8ad92
kashifaliawan <kashifalismitofficial@gmail.com>
2025-10-07 16:23:09 +0500

    updated

--- local_Storage/app.js
+++ local_Storage/app.js
@@ -1,9 +1,57 @@
+
+
 let r_name = document.querySelector("#r-name");
 let r_email = document.querySelector("#r-email");
 let r_password = document.querySelector("#r-password");
-// Login Form
-let l_password = document.querySelector("#l-password");
-let l_email = document.querySelector("#l-email");
+
+function getUsers() {
+    let users = localStorage.getItem("users");
+    if (users) {
+        users = JSON.parse(users)
+    } else {
+        users = [];
+    }
+    return users;
+}
+function register() {
+    if (r_email.value === '' || r_name.value === '' || r_password.value === '') {
+        alert("please fill the form ")
+    } else {
+        var users = getUsers()
+        let user = {
+            name: r_name.value,
+            email: r_email.value,
+            password: r_password.value,
+        }
+        users.push(user)
+        console.log(users);
+        localStorage.setItem("users", JSON.stringify(users))
+    }
+
+}
+
+function loginForm() {
+    let l_password = document.querySelector("#l-password");
+    let l_email = document.querySelector("#l-email");
+    var users = getUsers()
+    let saveUser = {}
+    for (let i = 0; i < users.length; i++) {
+        if (l_email.value == users[i].email) {
+            saveUser = users[i]
+            break;
+        }
+    }
+    if (saveUser.email) {
+        if (l_password.value == saveUser.password) {
+            console.log("login succesfully");
+        } else {
+            console.log("Encorrect Password");
+        }
+    }
+    else {
+        console.log("user Not Found");
+
+    }
 
 
-r_email.
+}
